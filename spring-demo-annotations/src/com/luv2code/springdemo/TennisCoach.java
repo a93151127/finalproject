ce84e018ace3efe919f0fd1ec8a652437f915a4f
Administrator <Administrator@DESKTOP-C73VVDS>
2021-01-28 01:36:10 +0800

    bean scope with java annotation

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,7 +1,11 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +17,16 @@ public class TennisCoach implements Coach {
 	
 	public TennisCoach() {
 		System.out.println("inside the constructor");
+	}
+	//define init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("inside init method");
+	}
+	//defin destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println("inside destroy method");
 	}
 	
 	/*@Autowired
